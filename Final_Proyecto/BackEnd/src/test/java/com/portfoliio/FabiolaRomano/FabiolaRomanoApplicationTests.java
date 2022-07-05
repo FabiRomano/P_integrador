fd1d3e52a245a3c1b819e511cc306685d95cde7b
FabiRomano <99614961+FabiRomano@users.noreply.github.com>
2022-07-05 20:41:36 -0300

    arreglos

---
+++ Final_Proyecto/BackEnd/src/test/java/com/portfoliio/FabiolaRomano/FabiolaRomanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliio.FabiolaRomano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabiolaRomanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
